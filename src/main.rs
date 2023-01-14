ff712006ae74cdc2587e68859a221618b7c34505
Kurbatov <kurbatxx@gmail.com>
2023-01-14 14:29:40 +0600

    minimal post

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,44 @@
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, post},
+    Json, Router,
+};
+use local_ip_address::local_ip;
+use serde_derive::Deserialize;
+use std::time::Duration;
+use std::{fs, net::SocketAddr};
+use tokio::time::sleep;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(hello));
+    //get ip
+    let addr = format!("{}:{}", local_ip().unwrap(), 3000);
+    let socket_address: SocketAddr = addr.parse().expect("НЕПРАВИЛЬНЫЙ АДРЕС");
 
-    // run it with hyper on localhost:3000
-    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
+    //paths
+    let app = Router::new()
+        .route("/", get(hello))
+        .route("/message", post(send_message));
+
+    //run server
+    println!("Running on {}", &socket_address);
+    axum::Server::bind(&socket_address)
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
+#[derive(Deserialize)]
+struct CreateMessage {
+    message: String,
+}
+
 async fn hello() -> &'static str {
-    return "Hello World";
+    sleep(Duration::from_millis(300)).await;
+    return "Hello World\n";
+}
+
+async fn send_message(Json(payload): Json<CreateMessage>) -> &'static str {
+    sleep(Duration::from_millis(500)).await;
+    println!("{}", payload.message);
+    return "message\n";
 }
