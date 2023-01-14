66b99cd1d20f2062b68e39306cd357b6a042197d
Kurbatov <kurbatxx@gmail.com>
2023-01-14 17:42:00 +0600

    parse config

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,46 @@ use axum::{
 };
 use local_ip_address::local_ip;
 use serde_derive::Deserialize;
-use std::time::Duration;
 use std::{fs, net::SocketAddr};
+use std::{process, time::Duration};
 use tokio::time::sleep;
+
+const CONFIG_DATA: &str = r#""port" = 3000
+"auth" = ""
+
+"id" = 0
+"token" = ""
+"#;
 
 #[tokio::main]
 async fn main() {
+    //get config
+    let config_file = match fs::read_to_string("config.toml") {
+        Ok(file) => file,
+        Err(_) => {
+            fs::write("./config.toml", CONFIG_DATA)
+                .expect("НЕ УДАЛОСЬ ЗАПИСАТЬ СОЗДАТЬ config.toml, ПРОВЕРЬТЕ ПРАВА");
+            println!("Заполни config файл и перезапусти службу\n");
+
+            process::exit(exitcode::OK);
+        }
+    };
+
+    let config: Config = toml::from_str(&config_file).expect("НЕПРАВИЛЬНО ЗАПОЛНЕН config.toml");
+    if config.auth.is_empty() || config.token.is_empty() || config.id == 0 || config.port == 0 {
+        println!("Все поля config файла должны быть заполнены\n");
+
+        process::exit(exitcode::OK);
+    }
+
     //get ip
-    let addr = format!("{}:{}", local_ip().unwrap(), 3000);
+    let addr = format!("{}:{}", local_ip().unwrap(), config.port);
     let socket_address: SocketAddr = addr.parse().expect("НЕПРАВИЛЬНЫЙ АДРЕС");
 
     //paths
     let app = Router::new()
         .route("/", get(hello))
+        .route("/exit", get(exit))
         .route("/message", post(send_message));
 
     //run server
@@ -27,8 +54,17 @@ async fn main() {
         .unwrap();
 }
 
+#[derive(Deserialize, Clone, Debug)]
+struct Config {
+    port: u32,
+    auth: String,
+    id: i32,
+    token: String,
+}
+
 #[derive(Deserialize)]
 struct CreateMessage {
+    auth: String,
     message: String,
 }
 
@@ -37,8 +73,16 @@ async fn hello() -> &'static str {
     return "Hello World\n";
 }
 
-async fn send_message(Json(payload): Json<CreateMessage>) -> &'static str {
+async fn send_message(Json(payload): Json<CreateMessage>, config: Config) -> &'static str {
     sleep(Duration::from_millis(500)).await;
+    if payload.auth == config.auth {
+        return "Неправильный код авторизации\n";
+    }
+
     println!("{}", payload.message);
     return "message\n";
+}
+
+async fn exit() {
+    process::exit(exitcode::OK);
 }
