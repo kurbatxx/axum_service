9d742d5bf9682e7a5e9b4b34564aa23d2e41778e
kurbatov <kurbatxx@gmail.com>
2023-06-22 15:53:08 +0600

    add info path

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use axum::{
 };
 use local_ip_address::local_ip;
 use reqwest::Client;
-use serde_derive::Deserialize;
+use serde::{Deserialize, Serialize};
 use std::{
     fs::{self, OpenOptions},
     net::SocketAddr,
@@ -96,6 +96,7 @@ async fn main() {
     //paths
     let app = Router::new()
         .route("/message", post(send_message))
+        .route("/info", post(info))
         .with_state(server_state)
         .route("/", get(check))
         .route("/exit", get(exit));
@@ -123,7 +124,7 @@ async fn main() {
 struct Config {
     port: u32,
     auth: String,
-    id: i32,
+    id: i64,
     token: String,
 }
 
@@ -131,6 +132,12 @@ struct Config {
 struct CreateMessage {
     auth: String,
     message: String,
+}
+
+#[derive(Deserialize, Serialize)]
+struct MessageInfo {
+    auth: String,
+    message: Vec<Instance>,
 }
 
 #[derive(Clone, Debug)]
@@ -148,8 +155,6 @@ async fn send_message(
     State(state): State<ServerState>,
     Json(payload): Json<CreateMessage>,
 ) -> &'static str {
-    //sleep(Duration::from_millis(500)).await;
-
     let config = state.config;
     if payload.auth != config.auth {
         return "Неправильный код авторизации\n";
@@ -169,6 +174,8 @@ async fn send_message(
         .send()
         .await;
 
+    dbg!(&req);
+
     match req {
         Ok(_) => {
             info!("{}", payload.message);
@@ -183,4 +190,76 @@ async fn send_message(
 
 async fn exit() {
     process::exit(exitcode::OK);
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Instance {
+    instance: String,
+    backups: Vec<Backup>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Backup {
+    pub id: String,
+    #[serde(rename(deserialize = "recovery-time"))]
+    pub recovery_time: String,
+
+    #[serde(rename(deserialize = "backup-mode"))]
+    pub backup_mode: String,
+    pub status: String,
+}
+
+async fn info(State(state): State<ServerState>, Json(payload): Json<MessageInfo>) -> &'static str {
+    let config = state.config;
+    if payload.auth != config.auth {
+        return "Неправильный код авторизации\n";
+    }
+
+    let v_st_instances: Vec<String> = payload.message
+                                .iter()
+                                .map(|x| {
+                                    let v_strings: Vec<String> = x
+                                        .backups
+                                        .iter()
+                                        .map(|b| {
+                                            format!(
+                                                // %0A == \n
+                                                "-------------------------------------------------------%0Aid: {}%0Arecovery time: {}%0Abackup mode: {}%0Astatus: {}%0A",
+                                                b.id,
+                                                b.recovery_time,
+                                                b.backup_mode,
+                                                b.status
+                                            )
+                                        })
+                                        .collect();
+
+                                    format!("Instance: <b>{}</b>%0A{}", x.instance, v_strings.join("\n"))
+                                })
+                                .collect();
+
+    for text in v_st_instances {
+        let req = state
+            .client
+            .get(
+                format!(
+                    "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}&parse_mode=html",
+                    config.token, config.id, text,
+                )
+                .as_str(),
+            )
+            .send()
+            .await;
+
+        match req {
+            Ok(_) => {
+                info!("{}", serde_json::to_string(&payload.message).unwrap());
+                return "Отправлено!\n";
+            }
+            Err(err) => {
+                warn!("{:?}", err);
+                return "Не отправлено!\n";
+            }
+        }
+    }
+    return "Json Обрaботан\n";
 }
